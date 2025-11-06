777e01a645c8a3887098d8e955324b6e93e8018f
Scetrr <zariftausif@gmail.com>
2025-11-07 00:36:59 +0600

    Solution.java

---
+++ Solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+        while (left < right) {
+            int width = right - left;
+            int high = Math.min(height[left], height[right]);
+            int area = width * high;
+            maxArea = Math.max(maxArea, area);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+}
